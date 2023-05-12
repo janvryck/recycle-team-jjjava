13bc464439e90a589b154b7650c3d83cbd309b4e
Jan Van Ryckeghem <jan@tabs-spaces.be>
2023-05-12 15:12:31 +0200

    Generify test assertions

--- src/test/java/com/dddeurope/recycle/spring/MainControllerTest.java
+++ src/test/java/com/dddeurope/recycle/spring/MainControllerTest.java
@@ -10,11 +10,13 @@ import com.dddeurope.recycle.events.IdCardRegistered;
 import com.dddeurope.recycle.events.IdCardScannedAtEntranceGate;
 import com.dddeurope.recycle.events.IdCardScannedAtExitGate;
 import com.dddeurope.recycle.events.PriceWasCalculated;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
 import java.util.UUID;
+import java.util.function.Function;
 
 class MainControllerTest {
 
@@ -30,7 +32,7 @@ class MainControllerTest {
 
         ResponseEntity<EventMessage> response = controller.handle(request);
 
-        assertCalculatedPrice(response, 0.0);
+        assertPriceWasCalculated(response, PriceWasCalculated::amount, 0.0);
     }
 
     @Test
@@ -44,7 +46,7 @@ class MainControllerTest {
 
         ResponseEntity<EventMessage> response = controller.handle(request);
 
-        assertCalculatedPrice(response, 10.65);
+        assertPriceWasCalculated(response, PriceWasCalculated::amount, 10.65);
     }
 
     @Test
@@ -59,7 +61,7 @@ class MainControllerTest {
 
         ResponseEntity<EventMessage> response = controller.handle(request);
 
-        assertCalculatedPrice(response, 9.72);
+        assertPriceWasCalculated(response, PriceWasCalculated::amount, 9.72);
     }
 
     @Test
@@ -74,22 +76,23 @@ class MainControllerTest {
 
         ResponseEntity<EventMessage> response = controller.handle(request);
 
-        assertCalculatedPrice(response, 0.0);
+        assertPriceWasCalculated(response, PriceWasCalculated::amount, 0.0);
     }
 
     @Test
     public void scenario_4_2() {
         MainController.RecycleRequest request = toRequest(
-            new IdCardRegistered("123", "Eric Cartman", "Point Dume", "South Park"),
-            new IdCardScannedAtEntranceGate("123", "2023-02-10"),
-            new FractionWasDropped("123", "Construction waste", 151),
-            new FractionWasDropped("123", "Green waste", 73),
-            new IdCardScannedAtExitGate("123")
+            new IdCardRegistered("567", "Eric Cartman", "Point Dume", "South Park"),
+            new IdCardScannedAtEntranceGate("567", "2023-02-10"),
+            new FractionWasDropped("567", "Construction waste", 151),
+            new FractionWasDropped("567", "Green waste", 73),
+            new IdCardScannedAtExitGate("567")
         );
 
         ResponseEntity<EventMessage> response = controller.handle(request);
 
-        assertCalculatedPrice(response, 11.94);
+        assertPriceWasCalculated(response, PriceWasCalculated::cardId, "567");
+        assertPriceWasCalculated(response, PriceWasCalculated::amount, 11.94);
     }
 
     private MainController.RecycleRequest toRequest(Event... events) {
@@ -100,11 +103,12 @@ class MainControllerTest {
         );
     }
 
-    private static void assertCalculatedPrice(ResponseEntity<EventMessage> handle, double expected) {
+    private static <T> void assertPriceWasCalculated(ResponseEntity<EventMessage> handle, Function<PriceWasCalculated, T> producer, T expected) {
         assertThat(handle.getBody())
             .isInstanceOfSatisfying(EventMessage.class, event -> assertThat(event.getPayload())
                 .isInstanceOfSatisfying(PriceWasCalculated.class, priceWasCalculated ->
-                    assertThat(priceWasCalculated.amount()).isEqualTo(expected)
+                    assertThat(producer.apply(priceWasCalculated)).isEqualTo(expected)
                 ));
     }
+
 }
